47a71f3f4f03e8c0d0d4731adb44916405189886
KyraCh <92988384+KyraCh@users.noreply.github.com>
2023-03-27 02:46:07 +0100

    Add files via upload

---
+++ build/precache-manifest.240571805695925fa622ade6ef183ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794ae8f9f79d741ca1520f65fd34f4b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2ed0c2fc5f2c4852cd6",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "5f7d1f7eeacf4096b95f",
+    "url": "/static/css/main.0d697476.chunk.css"
+  },
+  {
+    "revision": "c2ed0c2fc5f2c4852cd6",
+    "url": "/static/js/2.0b9be6a7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0b9be6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f7d1f7eeacf4096b95f",
+    "url": "/static/js/main.4d227601.chunk.js"
+  },
+  {
+    "revision": "6d50a49de8feb8d283f6",
+    "url": "/static/js/runtime-main.583b2c4a.js"
+  }
+]);
